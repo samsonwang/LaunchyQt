bc5e76a470ab73022c9d57bd3761880063b24005
SamsonWang <wangzhilv@gmail.com>
2018-10-24 17:20:49 +0800

    put dump file in temp Launchy dir with timestramp

--- src/win/minidump.cpp
+++ src/win/minidump.cpp
@@ -24,104 +24,124 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 TCHAR* MiniDumper::m_appName;
 MINIDUMPWRITEDUMP MiniDumper::m_dumpFunction;
 
-
-MiniDumper::MiniDumper(const TCHAR* appName)
-{
-	if (m_appName == NULL)
-	{
-		m_appName = _tcsdup(appName);
+MiniDumper::MiniDumper(const TCHAR* appName) {
+    if (m_appName == NULL) {
+        m_appName = _tcsdup(appName);
 #ifndef DEBUG
-		SetUnhandledExceptionFilter(TopLevelFilter);
+        SetUnhandledExceptionFilter(TopLevelFilter);
 #endif
-	}
+    }
 }
 
 
 LONG MiniDumper::TopLevelFilter(struct _EXCEPTION_POINTERS *exceptionInfo)
 {
-	LONG result = EXCEPTION_CONTINUE_SEARCH;
+    LONG result = EXCEPTION_CONTINUE_SEARCH;
 
-	if (CreateMiniDump(_T("Mini"), exceptionInfo, MiniDumpNormal) &&
-		CreateMiniDump(_T("Midi"), exceptionInfo, MiniDumpWithPrivateReadWriteMemory))
-	{
-		MessageBox(NULL,
-			_T("Sorry, Launchy seems to have crashed. To help us work out what went wrong, crash dumps have been created in your temp directory. Please send them to us via the SourceForge forums."),
-			m_appName, MB_OK | MB_ICONEXCLAMATION);
-		result = EXCEPTION_EXECUTE_HANDLER;
-	}
+    if (CreateMiniDump(_T("Mini"), exceptionInfo, MiniDumpNormal) &&
+        CreateMiniDump(_T("Midi"), exceptionInfo, MiniDumpWithPrivateReadWriteMemory))
+    {
+        MessageBox(NULL,
+                   _T("Sorry, Launchy seems to have crashed. "
+                      "To help us work out what went wrong, "
+                      "crash dumps have been created in your temp directory. "
+                      "Please send them to us via the SourceForge forums."),
+                   m_appName, MB_OK | MB_ICONEXCLAMATION);
+        result = EXCEPTION_EXECUTE_HANDLER;
+    }
 
-	return result;
+    return result;
 }
 
 
-bool MiniDumper::CreateMiniDump(const TCHAR* postfix, struct _EXCEPTION_POINTERS *exceptionInfo, MINIDUMP_TYPE dumpType)
-{
-	bool result = false;
+bool MiniDumper::CreateMiniDump(const TCHAR* postfix,
+                                struct _EXCEPTION_POINTERS *exceptionInfo,
+                                MINIDUMP_TYPE dumpType) {
+    bool result = false;
 
-	MINIDUMPWRITEDUMP pDump = GetMiniDumpWriteFunction();
-	if (pDump)
-	{
-		TCHAR dumpPath[_MAX_PATH];
+    MINIDUMPWRITEDUMP pDump = GetMiniDumpWriteFunction();
+    if (pDump) {
+        TCHAR dumpPath[_MAX_PATH];
 
-		// work out a good place for the dump file
-		if (!GetTempPath(_MAX_PATH, dumpPath))
-			_tcscpy(dumpPath, _T("c:\\"));
+        // work out a good place for the dump file
+        if (!GetTempPath(_MAX_PATH, dumpPath))
+            _tcscpy(dumpPath, _T("c:\\"));
 
-		_tcscat(dumpPath, m_appName);
-		_tcscat(dumpPath, _T("_"));
-		_tcscat(dumpPath, postfix);
-		_tcscat(dumpPath, _T("CrashDump.dmp"));
+        _tcscat(dumpPath, m_appName);
+        if (DirectoryExists(dumpPath) == TRUE
+            || CreateDirectory(dumpPath, NULL) == TRUE) {
+            _tcscat(dumpPath, _T("\\"));
+        }
+        else {
+            _tcscat(dumpPath, _T("_"));
+        }
+        _tcscat(dumpPath, postfix);
+        _tcscat(dumpPath, _T("CrashDump"));
 
-		// create the file
-		HANDLE hFile = CreateFile(dumpPath, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS,
-								  FILE_ATTRIBUTE_NORMAL, NULL);
+        SYSTEMTIME time;
+        GetLocalTime(&time);
+        TCHAR timeStr[256];
+        GetDateFormat(LOCALE_USER_DEFAULT, 0,
+                      &time, L"yyyyMMdd", timeStr, sizeof(timeStr));
+        _tcscat(dumpPath, timeStr);
 
-		if (hFile != INVALID_HANDLE_VALUE)
-		{
-			_MINIDUMP_EXCEPTION_INFORMATION ExInfo;
-			ExInfo.ThreadId = GetCurrentThreadId();
-			ExInfo.ExceptionPointers = exceptionInfo;
-			ExInfo.ClientPointers = NULL;
+        GetTimeFormatEx(LOCALE_NAME_USER_DEFAULT, 0,
+                        &time, L"HHmmss", timeStr, sizeof(timeStr));
+        _tcscat(dumpPath, timeStr);
 
-			// write the dump
-			if (pDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, dumpType, &ExInfo, NULL, NULL))
-			{
-				result = true;
-			}
-			CloseHandle(hFile);
-		}
-	}
+        _tcscat(dumpPath, _T(".dmp"));
 
-	return result;
+        // create the file
+        HANDLE hFile = CreateFile(dumpPath, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS,
+                                  FILE_ATTRIBUTE_NORMAL, NULL);
+
+        if (hFile != INVALID_HANDLE_VALUE) {
+            _MINIDUMP_EXCEPTION_INFORMATION ExInfo;
+            ExInfo.ThreadId = GetCurrentThreadId();
+            ExInfo.ExceptionPointers = exceptionInfo;
+            ExInfo.ClientPointers = NULL;
+
+            // write the dump
+            if (pDump(GetCurrentProcess(), GetCurrentProcessId(),
+                      hFile, dumpType, &ExInfo, NULL, NULL)) {
+                result = true;
+            }
+            CloseHandle(hFile);
+        }
+    }
+
+    return result;
 }
 
 
-MINIDUMPWRITEDUMP MiniDumper::GetMiniDumpWriteFunction()
-{
-	if (!m_dumpFunction)
-	{
-		// Find dbghelp.dll and check it exports the MiniDumpWriteDump function
-		HMODULE hDll = NULL;
-		TCHAR szDbgHelpPath[_MAX_PATH];
+MINIDUMPWRITEDUMP MiniDumper::GetMiniDumpWriteFunction() {
+    if (!m_dumpFunction) {
+        // Find dbghelp.dll and check it exports the MiniDumpWriteDump function
+        HMODULE hDll = NULL;
+        TCHAR szDbgHelpPath[_MAX_PATH];
 
-		if (GetModuleFileName(NULL, szDbgHelpPath, _MAX_PATH))
-		{
-			TCHAR *pSlash = _tcsrchr(szDbgHelpPath, _T('\\'));
-			if (pSlash)
-			{
-				_tcscpy(pSlash+1, _T("DBGHELP.DLL"));
-				hDll = LoadLibrary(szDbgHelpPath);
-			}
-		}
+        if (GetModuleFileName(NULL, szDbgHelpPath, _MAX_PATH)) {
+            TCHAR *pSlash = _tcsrchr(szDbgHelpPath, _T('\\'));
+            if (pSlash) {
+                _tcscpy(pSlash+1, _T("DBGHELP.DLL"));
+                hDll = LoadLibrary(szDbgHelpPath);
+            }
+        }
 
-		if (hDll == NULL)
-		{
-			// load any version we can
-			hDll = LoadLibrary(_T("DBGHELP.DLL"));
-		}
+        if (hDll == NULL) {
+            // load any version we can
+            hDll = LoadLibrary(_T("DBGHELP.DLL"));
+        }
 
-		m_dumpFunction = hDll ? (MINIDUMPWRITEDUMP)GetProcAddress(hDll, "MiniDumpWriteDump") : NULL;
-	}
+        m_dumpFunction = hDll ? (MINIDUMPWRITEDUMP)GetProcAddress(hDll, "MiniDumpWriteDump") : NULL;
+    }
 
-	return m_dumpFunction;
+    return m_dumpFunction;
+}
+
+BOOL MiniDumper::DirectoryExists(LPCTSTR szPath) {
+    DWORD dwAttrib = GetFileAttributes(szPath);
+
+    return (dwAttrib != INVALID_FILE_ATTRIBUTES
+            && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
 }
--- src/win/minidump.h
+++ src/win/minidump.h
@@ -17,32 +17,27 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#ifndef MINIDUMP
-#define MINIDUMP
-
+#pragma once
 
 #include "dbghelp.h"
 
-
 // from dbghelp.h
-typedef BOOL (WINAPI *MINIDUMPWRITEDUMP)(HANDLE hProcess, DWORD dwPid, HANDLE hFile, MINIDUMP_TYPE DumpType,
-									CONST PMINIDUMP_EXCEPTION_INFORMATION ExceptionParam,
-									CONST PMINIDUMP_USER_STREAM_INFORMATION UserStreamParam,
-									CONST PMINIDUMP_CALLBACK_INFORMATION CallbackParam
-									);
+typedef BOOL(WINAPI *MINIDUMPWRITEDUMP)(HANDLE hProcess, DWORD dwPid, HANDLE hFile, MINIDUMP_TYPE DumpType,
+                                        CONST PMINIDUMP_EXCEPTION_INFORMATION ExceptionParam,
+                                        CONST PMINIDUMP_USER_STREAM_INFORMATION UserStreamParam,
+                                        CONST PMINIDUMP_CALLBACK_INFORMATION CallbackParam);
 
 class MiniDumper
 {
 public:
-	MiniDumper(const TCHAR* appName);
+    MiniDumper(const TCHAR* appName);
 private:
-	static LONG WINAPI TopLevelFilter(struct _EXCEPTION_POINTERS *exceptionInfo);
-	static MINIDUMPWRITEDUMP GetMiniDumpWriteFunction();
-	static bool CreateMiniDump(const TCHAR* postfix, struct _EXCEPTION_POINTERS *exceptionInfo, MINIDUMP_TYPE dumpType);
+    static LONG WINAPI TopLevelFilter(struct _EXCEPTION_POINTERS *exceptionInfo);
+    static MINIDUMPWRITEDUMP GetMiniDumpWriteFunction();
+    static bool CreateMiniDump(const TCHAR* postfix, struct _EXCEPTION_POINTERS *exceptionInfo, MINIDUMP_TYPE dumpType);
+    static BOOL DirectoryExists(LPCTSTR szPath);
 
-	static MINIDUMPWRITEDUMP m_dumpFunction;
-	static TCHAR* m_appName;
+    static MINIDUMPWRITEDUMP m_dumpFunction;
+    static TCHAR* m_appName;
 };
 
-
-#endif
