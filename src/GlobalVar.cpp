bf071d3d9d98ae0f4c8b659387d80abb5ea65f8e
SamsonWang <wangzhilv@gmail.com>
2018-11-30 22:06:49 +0800

    change version to 3.0.4

--- src/GlobalVar.cpp
+++ src/GlobalVar.cpp
@@ -25,8 +25,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 namespace launchy {
 
-const int LAUNCHY_VERSION = 303;
-const char* LAUNCHY_VERSION_STRING = "3.0.3";
+const int LAUNCHY_VERSION = 304;
+const char* LAUNCHY_VERSION_STRING = "3.0.4";
 
 const uint HASH_LAUNCHY = 0;
 const uint HASH_HISTORY = 1;
