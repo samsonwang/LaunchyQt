14c341823100ad0a48b2e7c1a76aaefeb966396e
SamsonWang <wangzhilv@gmail.com>
2018-12-03 14:55:52 +0800

    update pluginconf.py

--- src/pluginpy/pluginconf.py
+++ src/pluginpy/pluginconf.py
@@ -6,11 +6,11 @@ def redirectOutput():
     # If I write code such as sys.stdout = open('stdout.txt', 'w'),
     # I have to flush the file after writing to it (using "print" for example).
     # The following class saves the need to flush the file each time.
-
     class FlushedFile:
         """ Provides an output file that is immediately flushed after write """
         def __init__(self, filepath):
             try:
+                self.terminal = sys.stdout
                 self.file = open(filepath, 'w')
             except Exception:
                 self.file = None
@@ -21,6 +21,13 @@ def redirectOutput():
             self.file.write(str)
             self.file.flush()
 
+        def __getattr__(self, attr):
+            try:
+                self.file.flush()
+                return getattr(self.terminal, attr)
+            except Exception:
+                pass
+                #print ( sys.stderr, inst)
     # Redirect stdout and stderr
     sys.stdout = FlushedFile(os.path.join(launchy.getAppPath(), 'python', 'stdout.log'))
     sys.stderr = FlushedFile(os.path.join(launchy.getAppPath(), 'python', 'stderr.log'))
@@ -28,4 +35,78 @@ def redirectOutput():
 
 redirectOutput()
 
+#import sys
+print("sys.prefix:", sys.prefix)
+xlib = os.path.join(sys.prefix, 'Lib')
+sys.path.insert(0, xlib)
+sys.path.insert(0, sys.prefix)
+sys.path.insert(0, ".")
 
+#path = os.environ.get('PATH', '')
+#print ("env.path(origin):", path)
+
+#os.environ['PATH'] = path + os.pathsep + sys.prefix #+ os.pathsep
+
+print ("sys.path1:", sys.path)
+print ("env.path1:", os.environ.get('PATH', ''))
+
+try:
+    import site
+    site.main()
+except Exception:
+    print("fail to import site")
+    pass
+
+print ("sys.path2:", sys.path)
+print ("env.path2:", os.environ.get('PATH', ''))
+
+os.chdir(sys.prefix)
+
+"""
+def setSettingsObject():
+    # Set the launchy.settings object
+    try:
+        # Based on http://lists.kde.org/?l=pykde&m=108947844203156&w=2
+        from PyQt5 import QtCore
+        from sip import wrapinstance, unwrapinstance
+        from PyQt5.QtWidgets import QApplication, QWidget
+        print ("QApplication:", QApplication)
+        print ("QWidget:", QWidget)
+        print ("QtCore:", QtCore)
+        print ("launchy:", dir(launchy))
+        launchy.settings =  wrapinstance(launchy.__settings, QtCore.QSettings)
+        print ( launchy.settings )
+    except (ImportError, inst):
+        print ( sys.stderr, inst)
+    except NameError:
+        print (sys.stderr, "Could not find __settings object")
+    except (Exception, inst):
+        print ( sys.stderr, inst)
+
+setSettingsObject()
+
+
+from PyQt5.QtWidgets import (QWidget, QToolTip,
+    QPushButton, QApplication)
+from PyQt5.QtGui import QFont
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')
+        self.show()
+ex = Example()
+"""
