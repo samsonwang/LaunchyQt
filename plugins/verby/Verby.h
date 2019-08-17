68fe5c872709a531ed74f7969abdeba364735086
SamsonWang <wangzhilv@gmail.com>
2019-08-17 09:33:43 +0800

    prepare to release 3.1.0

--- plugins/verby/Verby.h
+++ plugins/verby/Verby.h
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 class Gui;
 
-class Verby : public QObject, public launchy::PluginInterface{
+class Verby : public QObject, public launchy::PluginInterface {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID PLUGININTERFACE_IID)
     Q_INTERFACES(launchy::PluginInterface)
