89401996c4843000b5a566292811f66f713f8598
SamsonWang <wangzhilv@gmail.com>
2020-01-29 13:45:42 +0800

    prepare tag v3.1.1

--- src/GlobalVar.cpp
+++ src/GlobalVar.cpp
@@ -44,8 +44,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 namespace launchy {
 
-const int LAUNCHY_VERSION = 310;
-const char* LAUNCHY_VERSION_STRING = "3.1.0";
+const int LAUNCHY_VERSION = 311;
+const char* LAUNCHY_VERSION_STRING = "3.1.1";
 
 #if defined(ENVIRONMENT64)
 const char* LAUNCHY_BIT_STRING = "64";
